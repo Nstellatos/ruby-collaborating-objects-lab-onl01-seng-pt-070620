24712668fef297d421702790b7837ec51c8e3025
Nicole Stellatos <stellatosnicole@gmail.com>
2020-08-03 02:29:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -8,8 +8,13 @@ end
   def self.all
     @@all
   end
-  def self.new_by_filename
-end
+  def self.new_by_filename(file_name)
+    song = file_name.split(" - ")[1]
+    artist = file_name.split(" - ")[0]
+    new_song = self.new(song)
+    new_song.artist_name = artist
+    new_song
+  end
   
   
   
