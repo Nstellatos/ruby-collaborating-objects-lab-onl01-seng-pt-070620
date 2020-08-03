74df9f8d3d22cfc5ee0c41670c41f2c9cc384b9e
Nicole Stellatos <stellatosnicole@gmail.com>
2020-08-03 02:44:36 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,11 @@ end
     song.artist_name = artist
     song
   end
-  
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+    self.artist.add_song(self)
+  end
+
 
     
     
